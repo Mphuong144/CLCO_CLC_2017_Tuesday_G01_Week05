a9321368e12d2c89d86323cc53c1cc6009f7fce9
dongockhanh3103 <dongockhanh3103@gmail.com>
2017-03-18 17:55:31 +0700

    push code cloud week 5

---
+++ GAEJXMPPTutorial/src/com/gaejexperiments/xmpptutorial/week5.java
@@ -0,0 +1,35 @@
+package com.gaejexperiments.xmpptutorial;
+
+public class week5 {
+	
+}
+
+class DataClass implements AddF{
+
+	@Override
+	public void bublesort() {
+		
+	}
+
+	@Override
+	public void quicksort() {
+	
+	}
+
+	@Override
+	public void nearlysort() {
+		
+	}
+	
+	
+}
+
+interface AddF{
+	
+	void bublesort();
+	void quicksort();
+	void nearlysort();
+	
+}
+
+
